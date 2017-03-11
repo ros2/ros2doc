5b372c78fe0c0f2ce0febc0d5fed94c349d93dce
Morgan Quigley <morgan@osrfoundation.org>
2017-03-11 12:40:30 -0800

    generate docs only for specific packages
    refactor to use argparse, so that you can either generate docs for the entire
    source tree, or just specific ones

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ i can haz docs?
 # installation
 sudo apt install python3-sphinx python3-jinja2 python3-breathe
 
+# running it
+
+## to generate docs for all packages
+. ~/ros2/install/setup.bash
+./ros2doc.py ../src --all
+
+## to generate docs just for a few specific packages:
+. ~/ros2/install/setup.bash
+./ros2doc.py ../src rmw rcl
+
 # serving locally
 sudo apt-get install lighttpd
 sudo ln -s `pwd`/docbuild/latest/html /var/www/html/latest
--- ros2doc.py
+++ ros2doc.py
@@ -2,6 +2,7 @@
 import ament_package
 import ament_tools.verbs.list_packages
 from ament_tools.package_types import parse_package
+import argparse
 import datetime
 import errno
 import jinja2
@@ -31,85 +32,109 @@ def jinja_autoescape(template_name):
     if template_name.endswith(('.html')):
         return True
 
-def awesome():
-    ros2doc_path = os.path.abspath('.')  # yeah that's probably not great
-    jinja_env = jinja2.Environment(loader=jinja2.PackageLoader('ros2doc', 'templates'), autoescape=jinja_autoescape)
-
-    dt = datetime.datetime.now()
-
-    buildpath = os.path.abspath(os.path.join('docbuild', dt.strftime('%Y%m%d.%H%M%S')))
-    os.makedirs(buildpath)
-    update_symlink(buildpath, os.path.join('docbuild', 'latest'))
-    
-    pkgs_doc_path = os.path.join(buildpath, 'pkgs')
-    os.makedirs(pkgs_doc_path)
-    
-    html_output_path = os.path.abspath(os.path.join(buildpath, 'html'))
-    os.makedirs(html_output_path)
-   
+def parse_pkgs():
     basepath = sys.argv[1]
     print("running on sourcepath {0}".format(basepath))
     pkg_paths = ament_tools.verbs.list_packages.find_package_paths(basepath)
-    pkg_names = [ ]
-
+    pkgs = [ ]
     for pkg_path in pkg_paths:
-        pkg_abspath = os.path.join(basepath, pkg_path)
-        pkg = parse_package(pkg_abspath)
-        pkg_names.append([pkg.name, False])
-        #if pkg.name != 'joy':
-        #    continue
-        pkg_copy_path = os.path.join(pkgs_doc_path, pkg.name)
-        if os.path.exists(pkg_copy_path):
-            print("copy path exists")
-            continue
-        shutil.copytree(pkg_abspath, pkg_copy_path)
-        doc_path = os.path.join(pkg_copy_path, 'doc')
-        print("looking in %s" % doc_path)
-        if not os.path.exists(doc_path):
-            os.makedirs(doc_path)
-        if True:
-            print("doc path exists")
-            pkg_html_output_path = os.path.join(html_output_path, pkg.name)
-            os.makedirs(pkg_html_output_path)
-            # hooray there is a doc directory. let's create a conf.py in there
-            doxygen_conf_path = os.path.join(doc_path, 'doxygen.conf')
-            doxygen_conf_template = jinja_env.get_template('doxygen.conf')
-            doxygen_conf_file = open(doxygen_conf_path, 'w')
-            doxygen_conf_file.write(doxygen_conf_template.render(pkg_name=pkg.name))
-            doxygen_conf_file.close()
-            run_shell_command("doxygen doxygen.conf", doc_path)
+        pkg_dict = { }
+        pkg_dict['abspath'] = os.path.join(basepath, pkg_path)
+        pkg_dict['parsed'] = parse_package(pkg_dict['abspath'])
+        pkg_dict['name'] = pkg_dict['parsed'].name
+        pkg_dict['generated'] = False
+        pkgs.append(pkg_dict)
+    return pkgs
 
-            doxygen_path = os.path.join(doc_path, 'doxygen', 'xml')
+def generate_pkg_doc(pkgs_doc_path, pkgs_html_root, pkg, jinja_env):
+    # first, we need to copy the sources somewhere temporary because we'll
+    # probably generate debris that we don't want to clutter up the source tree
+    pkg_copy_path = os.path.join(pkgs_doc_path, pkg['name'])
+    if os.path.exists(pkg_copy_path):
+        print("copy path exists")
+        return
 
-            sphinx_index_filename = os.path.join(doc_path, 'index.rst')
-            if not os.path.exists(sphinx_index_filename):
-                print("creating sphinx index file at {0}".format(sphinx_index_filename))
-                sphinx_index_template = jinja_env.get_template('index.rst')
-                sphinx_index_file = open(sphinx_index_filename, 'w')
-                sphinx_index_file.write(sphinx_index_template.render(pkg_name=pkg.name))
-                sphinx_index_file.close()
+    shutil.copytree(pkg['abspath'], pkg_copy_path)
+    doc_path = os.path.join(pkg_copy_path, 'doc')
+    print("looking in %s" % doc_path)
+    if not os.path.exists(doc_path):
+        os.makedirs(doc_path)
 
-            conf_py_path = os.path.join(doc_path, 'conf.py')
-            print("hai i will now create a conf.py at %s" % conf_py_path)
-            conf_py_file = open(conf_py_path, 'w')
-            conf_py_template = jinja_env.get_template('conf.py')
-            conf_py_file.write(conf_py_template.render(pkg_name=pkg.name, doxygen_path=doxygen_path))
-            conf_py_file.close()
-            #run_shell_command("sphinx-build -b html . {0}".format(pkg_html_output_path), doc_path)
-            run_shell_command("{0}/sphinx3-build -b html . {1}".format(ros2doc_path, pkg_html_output_path), doc_path)
-            pkg_names[-1][1] = True  # we've built docs for this pkg
+    print("doc path exists")
+    pkg_html_output_path = os.path.join(pkgs_html_root, pkg['name'])
+    os.makedirs(pkg_html_output_path)
+    # hooray there is a doc directory. let's create a conf.py in there
+    doxygen_conf_path = os.path.join(doc_path, 'doxygen.conf')
+    doxygen_conf_template = jinja_env.get_template('doxygen.conf')
+    doxygen_conf_file = open(doxygen_conf_path, 'w')
+    doxygen_conf_file.write(doxygen_conf_template.render(pkg_name=pkg['name']))
+    doxygen_conf_file.close()
+    run_shell_command("doxygen doxygen.conf", doc_path)
 
-    pkg_names.sort()
+    doxygen_path = os.path.join(doc_path, 'doxygen', 'xml')
+
+    sphinx_index_filename = os.path.join(doc_path, 'index.rst')
+    if not os.path.exists(sphinx_index_filename):
+        print("creating sphinx index file at {0}".format(sphinx_index_filename))
+        sphinx_index_template = jinja_env.get_template('index.rst')
+        sphinx_index = open(sphinx_index_filename, 'w')
+        sphinx_index.write(sphinx_index_template.render(pkg_name=pkg['name']))
+        sphinx_index.close()
+
+    conf_py_path = os.path.join(doc_path, 'conf.py')
+    print("hai i will now create a conf.py at %s" % conf_py_path)
+    conf_py_file = open(conf_py_path, 'w')
+    conf_py_template = jinja_env.get_template('conf.py')
+    conf_py_file.write(conf_py_template.render(pkg_name=pkg['name'], doxygen_path=doxygen_path))
+    conf_py_file.close()
+    #run_shell_command("sphinx-build -b html . {0}".format(pkg_html_output_path), doc_path)
+    ros2doc_path = os.path.abspath('.')  # yeah that's probably not great
+    run_shell_command("{0}/sphinx3-build -b html . {1}".format(ros2doc_path, pkg_html_output_path), doc_path)
+    pkg['generated'] = True  # we've built docs for this pkg
+ 
+
+def make_index(index_filename, pkgs, jinja_env):
     # make the index file
+    #jinja_env = jinja2.Environment(loader=jinja2.PackageLoader('ros2doc', 'templates'), autoescape=jinja_autoescape)
     index_template = jinja_env.get_template('index.html')
-    index_file_name = os.path.join(html_output_path, 'index.html')
-    print("creating index file at {0}".format(index_file_name))
-    index_file = open(index_file_name, 'w')
-    index_file.write(index_template.render(pkg_names=pkg_names))
+    print("creating index file at {0}".format(index_filename))
+    index_file = open(index_filename, 'w')
+    index_file.write(index_template.render(pkgs=pkgs))
     index_file.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("usage: ros2doc SOURCEPATH")
-        sys.exit(1)
-    awesome()
+    parser = argparse.ArgumentParser(description="Let's generate some docs!")
+    parser.add_argument('src_path', metavar='SRC_PATH', type=str, nargs=1,
+                        help='ROS 2 source tree')
+    parser.add_argument('pkg', metavar='PKG', type=str, nargs='*',
+                        help='package(s) to build docs')
+    parser.add_argument('--all', action='store_true',
+                        help='generate docs for all packages')
+    args = parser.parse_args()
+    print(args)
+
+    jinja_env = jinja2.Environment(loader=jinja2.PackageLoader('ros2doc', 'templates'), autoescape=jinja_autoescape)
+    now = datetime.datetime.now()
+    buildpath = os.path.abspath(os.path.join('docbuild', now.strftime('%Y%m%d.%H%M%S')))
+    os.makedirs(buildpath)
+    update_symlink(buildpath, os.path.join('docbuild', 'latest'))
+
+    pkgs_doc_path = os.path.join(buildpath, 'pkgs')
+    os.makedirs(pkgs_doc_path)
+
+    html_output_path = os.path.abspath(os.path.join(buildpath, 'html'))
+    os.makedirs(html_output_path)
+
+    pkgs = parse_pkgs()
+    print("found {0} packages".format(len(pkgs)))
+
+    for pkg in pkgs:
+        if args.all or (pkg['name'] in args.pkg):
+            generate_pkg_doc(pkgs_doc_path, html_output_path, pkg, jinja_env)
+
+    index_filename = os.path.join(html_output_path, 'index.html')
+    make_index(index_filename, pkgs, jinja_env)
+    '''
+    #pkg_names.sort()
+
+    '''
